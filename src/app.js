d664f836c348664f1e1f1a870c35c7dca3bfab9e
Dalip Tandon <daliptandon8@gmail.com>
2024-10-06 11:48:15 +0530

    create multiple routes

--- src/app.js
+++ src/app.js
@@ -2,18 +2,25 @@ const express=require("express");
 const app=express();
 
 
-app.get("/",(req,res)=>{
-    res.send("hello from server dashboard");
+app.get("/profile",(req,res,next)=>{
+    console.log("first route");
     
-})
-app.get("/profile",(req,res)=>{
-    res.send("hello from server profile");
+    // res.send("hello from server profile1");
+    next();
     
+},(req,res,next)=>{
+    console.log("second route");
+
+    // res.send("hello from server profile2");
+    next();
+
+},(req,res,next)=>{
+    console.log("third route");
+
+    res.send("hello from server profile3");
+
 })
-app.get("/demo",(req,res)=>{
-    res.send("hello from server demo");
-    
-})
+
 
 app.listen(3000,()=>{
     console.log("Server is started at port number 3000");
