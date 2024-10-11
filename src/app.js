6ff261503f9881d1188bf0b7665dbb35c1de4bbb
Dalip Tandon <daliptandon8@gmail.com>
2024-10-11 22:41:31 +0530

    create login api and hashpassword

--- src/app.js
+++ src/app.js
@@ -2,20 +2,54 @@ const express=require("express");
 const app=express();
 const connectDB=require("./config/databse");
 const User=require("./models/user");
+const {signupValidation}=require("./utils/validation");
 const { model } = require("mongoose");
+const bcrypt = require('bcrypt');
+const validator=require("validator");
 
 app.use(express.json());  //middleware to read  json data provided by express js
 app.post("/signup",async(req,res)=>{
    // console.log(req.body); //req.body is used to read data send via api 
-    const user=new User(req.body); //adding data via post api
     try{
+        signupValidation(req);
+        const{firstName,lastName,emailId,password}=req.body;
+        const hashPassword=await bcrypt.hash(password,10);
+        const user=new User({
+            firstName,lastName,emailId,password:hashPassword ,   //adding data via post api
+        });
         await user.save();
         res.send("new user is created");
     }catch(err){
-        res.status(400).send("contact support team"+err.message);
+        res.status(400).send("ERROR: "+err.message);
     }
 
 })
+
+
+app.post("/login",async(req,res)=>{
+    const{emailId,password}=req.body;
+    try{
+    if(!validator.isEmail(emailId)){
+        throw new Error("invalid email");
+    }
+    const user=await User.findOne({emailId:emailId});
+
+    if(!user){
+        throw new Error("Invalid credentails");
+    }
+    const isPassword=await bcrypt.compare(password,user.password);
+    if(!isPassword){
+        throw new Error("invalid credentails")
+    }else{
+        res.send("login successfull")
+    }
+    
+}catch(error){
+    res.status(400).send("Error: "+error.message);
+}
+})
+
+
 app.get("/user",async(req,res)=>{
     const userEmail=req.body.emailId;
     try{
@@ -30,6 +64,7 @@ app.get("/user",async(req,res)=>{
     }
 })
 
+
 app.get("/feed",async(req,res)=>{
     try{
         const users=await User.find({});
@@ -38,6 +73,8 @@ app.get("/feed",async(req,res)=>{
         res.status(404).send("something went wrong");
     }
 })
+
+
 app.delete("/user",async(req,res)=>{
     // const user=req.body.emailId; deleting user based on email id
     const user=req.body.userId;
@@ -51,6 +88,9 @@ app.delete("/user",async(req,res)=>{
         res.status(404).send("something went wrong");
     }
 })
+
+
+
 app.patch("/user/:userId",async(req,res)=>{
     const userId=req.params.userId;
     const data=req.body;
@@ -71,6 +111,9 @@ app.patch("/user/:userId",async(req,res)=>{
         res.status(404).send("Update failed :"+err.message);
     }
 })
+
+
+
 connectDB().then(()=>{
     console.log("DB is connected successfully");   
     app.listen(3000,()=>{
