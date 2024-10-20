d3fad79cc015d142cff8e4da6b279e2a4d34c972
Dalip Tandon <daliptandon8@gmail.com>
2024-10-20 14:36:01 +0530

    add pagination

--- src/routes/userRequest.js
+++ src/routes/userRequest.js
@@ -63,6 +63,10 @@ userRouter.get("/user/feed",userAuthentication,async(req,res)=>{
     try{
 
         const loggedInUser=req.user;
+        const page=parseInt(req.query.page) || 1;
+        let limit=parseInt(req.query.limit) || 10;
+        limit=limit>50?50:limit;
+        const skip=(page-1)*limit;
 
         const connectionRequests=await connectionRequest.find({
             $or:[
@@ -80,7 +84,7 @@ userRouter.get("/user/feed",userAuthentication,async(req,res)=>{
                 {_id:{$nin:Array.from(hideUserFromFeed)}},
                 {_id:{$ne:loggedInUser._id}}
             ]
-        }).select(SECRET_DATA);
+        }).select(SECRET_DATA).skip(skip).limit(limit);
 
         res.send(users);
 
